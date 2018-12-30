acac1dc99948002bdb0649b0d68f5ef93bec3bf6
Robert Banaszak <robert.banaszak1997@gmail.com>
2018-12-30 14:38:34 +0100

    fix listing rooms and participants

--- src/Kahoot.h
+++ src/Kahoot.h
@@ -38,7 +38,7 @@ public:
     void setTimer();
     int writeMessage(Client * client, std::string message);
     int receiveAnswer(Client * client, char * buffer);
-    void broadcastPlayersInRoom();
+    void sendPlayersInRoom(Client * client);
     float getRemainingTime();
 };
 
